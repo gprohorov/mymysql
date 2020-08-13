e029b7bee380191aa3404cf8c9809333f4074b1c
123456 <sasha.lazoryak@mail.ru>
2020-08-13 23:44:48 +0200

    crud

--- src/main/java/pro/edu/mymysql/MymysqlApplication.java
+++ src/main/java/pro/edu/mymysql/MymysqlApplication.java
@@ -2,8 +2,10 @@ package pro.edu.mymysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MymysqlApplication {
 
     public static void main(String[] args) {
--- src/main/java/pro/edu/mymysql/controller/rest/CategoryController.java
+++ src/main/java/pro/edu/mymysql/controller/rest/CategoryController.java
@@ -2,9 +2,7 @@ package pro.edu.mymysql.controller.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.edu.mymysql.Genre;
 import pro.edu.mymysql.CategoryServiceImpl;
 
@@ -17,13 +15,35 @@ public class CategoryController {
     @Autowired
     CategoryServiceImpl service;
 
-    @RequestMapping("/get/list")
+    @GetMapping("/")
     List<Genre> getAll(){
         return service.getAll();
     }
 
-    @RequestMapping("/get/{id}")
+    @GetMapping("/{id}")
     Genre get(@PathVariable("id") String id){
         return service.get(id);
     }
+
+    @PostMapping("/")
+    Genre create(@RequestBody Genre genre){
+        return service.create(genre);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    Genre update(@RequestBody Genre genre){
+        return service.create(genre);
+    }
+
+    @DeleteMapping("/{id}")
+    Genre delete(@PathVariable("id") String id) {
+        return service.delete(id);
+    }
+
 }
+/*
+* http://localhost:8080/v2/api-docs
+* http://localhost:8080/swagger-ui.html
+*
+*
+* */
